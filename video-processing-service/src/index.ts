fda28aa30924f9770062a52427f7df6413de3565
Dustin <dustinpham07@gmail.com>
2024-10-19 15:37:31 -0700

    Added services for google cloud to deploy video processing, pub/sub topic and subscriptions, and storage buckets for processed videos

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,35 +1,70 @@
 import express from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+
+import { 
+  uploadProcessedVideo,
+  downloadRawVideo,
+  deleteRawVideo,
+  deleteProcessedVideo,
+  convertVideo,
+  setupDirectories
+} from './storage';
+import { ALL } from 'dns';
+
+// Create the local directories for videos
+setupDirectories();
 
 const app = express();
-app.use(express.json()); //initialize use of json?
+app.use(express.json());
 
-app.post('/process-video', (req, res) => {
+app.post('/process-video', async (req, res) => {
 
-  // Get the path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
+  let data
 
-  // Check if the input file path is defined
-  if (!inputFilePath || !outputFilePath) {
-    res.status(400).send('Bad Request: Missing file path');
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8')
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error('Invalid message payload received.');
+    } 
+    }catch (error) {
+      console.error(error)
+      res.status(400).send('Bad request: missing filename.')
+      return      
   }
+  
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`
 
-  // Create the ffmpeg command
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360') // 360p
-    .on('end', () =>{
-        console.log('Processing finished successfully');
-        res.status(200).send('Processing finished successfully');
-    })
-    .on('error', (err) =>{
-        console.log('An error occurred: ' + err.message);
-        res.status(500).send('An error occurred: ' + err.message);
-    })
-    .save(outputFilePath);
+
+  await downloadRawVideo(inputFileName);
+
+  try {
+    await convertVideo(inputFileName, outputFileName)
+  } catch (error) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)      
+    ]);
+    res.status(500).send('Internal Server Error: video processing failed.')
+    return
+  
+  }
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+  res.status(200).send('Processing finished successfully');
+  return 
 });
 
-const port = process.env.PORT || 3000; //standard way to provide the port at runtime, otherwise use 3000 if undefined
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Video processing service listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
+
+
+
